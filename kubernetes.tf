05805a177d7b3bc8eaf18e505ae943c16ce418a2
Audy <amaudy@gmail.com>
2022-11-06 15:30:02 +0700

    Changed name of cluster

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,14 +1,14 @@
 resource "digitalocean_kubernetes_cluster" "sandboxk8s" {
-  name   = var.project_name
-  region = var.region
+  name     = var.project_name
+  region   = var.region
   vpc_uuid = digitalocean_vpc.sandboxk8s.id
   # Grab the latest version slug from `doctl kubernetes options versions`
   version = "1.24.4-do.0"
 
   node_pool {
     name = "sandbox-worker-pool"
-    # size       = "s-2vcpu-2gb"
-    size       = "s-2vcpu-4gb-amd"
+    size = "s-2vcpu-2gb"
+    # size       = "s-2vcpu-4gb-amd"
     node_count = 3
 
     taint {
